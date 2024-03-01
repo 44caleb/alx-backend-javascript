734b1963830c6435ed0ab9e833e1fcd99956dd80
44caleb <calebadinfono2@gmail.com>
2024-03-01 05:36:24 +0100

    unittest

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,8 +1,6 @@
-const OPERATIONS = {
-  SUM: 'SUM',
-  SUBTRACT: 'SUBTRACT',
-  DIVIDE: 'DIVIDE',
-};
+const SUM = 'SUM';
+const SUBTRACT = 'SUBTRACT';
+const DIVIDE = 'DIVIDE';
 
 function isNegZero(n) {
   n = Number(n);
@@ -10,33 +8,27 @@ function isNegZero(n) {
 }
 
 module.exports = function calculateNumber(type, a, b = 0) {
-  // Convert input parameters to numbers
   let aNum = Number(a);
   let bNum = Number(b);
 
-  // Check if the input parameters are valid numbers
-  if (Number.isNaN(aNum) || Number.isNaN(bNum)) {
+  if (Number.isNaN(aNum) || Number.isNaN(bNum))
     throw TypeError('Parameters must be numbers or able to coerce to number');
-  }
 
-  // Round the numbers to integers
   aNum = Math.round(aNum);
   bNum = Math.round(bNum);
 
-  // Perform the requested operation
   switch (type) {
-    case OPERATIONS.SUM:
+    case SUM:
       return aNum + bNum;
-    case OPERATIONS.SUBTRACT:
+    case SUBTRACT:
       return aNum - bNum;
-    case OPERATIONS.DIVIDE:
-      if (bNum === 0) {
-        return 'ERROR: Division by zero';
-      }
+    case DIVIDE:
+      if (bNum === 0) return 'ERROR';
       const quotient = aNum / bNum;
       return isNegZero(quotient) ? 0 : quotient;
     default:
-      throw Error('Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".');
+      throw Error(
+        'Invalid operation type. Valid types are "SUM", "SUBTRACT", and "DIVIDE".'
+      );
   }
 };
-
--- 0x06-unittests_in_js/2-calcul_chai.js
+++ 0x06-unittests_in_js/2-calcul_chai.js
@@ -1,22 +1,19 @@
 function calculateNumber(type, a, b) {
-  // Convert input parameters to numbers and round them
-  const n1 = Math.round(Number(a));
-  const n2 = Math.round(Number(b));
+  const n1 = Math.round(a);
+  const n2 = Math.round(b);
 
-  // Perform the requested operation based on the type
-  switch (type) {
-    case 'SUBTRACT':
-      return n1 - n2;
-    case 'DIVIDE':
-      // Check for division by zero
-      if (n2 === 0) {
-        return 'Error: Division by zero';
-      }
-      return n1 / n2;
-    default:
-      return n1 + n2;
+  if (type === 'SUBTRACT') {
+    return n1 - n2;
   }
+
+  if (type === 'DIVIDE') {
+    if (n2 === 0) {
+      return 'Error';
+    }
+    return n1 / n2;
+  }
+
+  return n1 + n2;
 }
 
 module.exports = calculateNumber;
-
